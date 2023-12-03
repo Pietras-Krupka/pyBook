f3f68d5c0f7f3c8bd3f850986425e1ea2292d901
Pietras-Krupka <pietras.natalia@gmail.com>
2023-12-03 12:42:01 +0100

    Stworzenie pierwszego testu

--- book.py
+++ book.py
@@ -4,7 +4,4 @@ class Book:
         self.author = author
         self.year = year
     def get_info(self):
-        return f"Książka: {self.title} Autor: {self.author} Rok: {self.year}"
-
-book = Book("Wiedźmin", "AS", 2008)
-print(book.get_info())
+        return f'Książka: {self.title} Autor: {self.author} Rok: {self.year}'
