0f50d5359186840d5c5080388e824f29d680651c
Pietras-Krupka <pietras.natalia@gmail.com>
2023-12-03 13:20:55 +0100

    Stworzenie testów dla autora i roku

--- book_test.py
+++ book_test.py
@@ -10,5 +10,15 @@ class testBook(unittest.TestCase):
         text_correct = 'Książka: Wiedźmin Autor: Sapkowski Rok: 2000'
         text_result = self.book.get_info()
         self.assertEqual(text_correct, text_result)
+    def test_change_title(self):
+        self.book.change_title('Miecz przeznaczenia')
+        self.assertEqual('Miecz przeznaczenia', self.book.title)
+    def test_change_author(self):
+        self.book.change_author('Andrzej Sapkowski')
+        self.assertEqual('Andrzej Sapkowski', self.book.author)
+    def test_change_year(self):
+        self.book.change_year(2000)
+        self.assertEqual(2000, self.book.year)
+
 if __name__ == '__main__':
     unittest.main()
